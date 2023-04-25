6b0772776de29acf918c797544897e4afb1a2760
nikhil-user123 <nikhilprajapat602@gmail.com>
2023-04-25 17:11:59 +0530

    feat:parallex effect footer is under construction

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,15 @@
 import React from 'react'
 import './navbar.css'
 // import {Link} from 'react'
-import { useEffect, useState } from 'react';
 
 
 export default function Home() {
-  const [scrolled, setScrolled] = useState(false);
-  const handleScroll = () => {
-    const offset = window.scrollY;
-    if (offset > 200) {
-      setScrolled(true);
-    }
-    else {
-      setScrolled(false);
-    }
-  }
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
-    console.log("i run")
-  })
-  let navbarClasses = ['navbar'];
-  if (scrolled) {
-    navbarClasses.push('scrolled');
-  }
+ 
   return (
-    <div className='div'>
+    <div >
       <header>
-        <div class="container-box">
-          <nav className={navbarClasses.join(" ")}>
+        <div class="container-box scrolled">
+          <nav className='navbar'>
             <a href="/" className="logo me-auto"><img src="/educational.jpg" alt="" className="img-fluid" /></a>
             <ul className='navbar_content'>
               <li><a class="current" href="/">Home</a></li>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,8 +3,10 @@ import './land.css'
 export default function Landing() {
     return (
         <div>
-            <section className="home" id="hero">
-                {/* <img src="https://thehubbackend.com/media/Learning-development-jobs.jpg" alt="" /> */}
+            <section className='home' >
+                <div id="hero"></div>
+                {/* <img alt='img' src="/public/educational-theme-close-up-student-writing-classroom-min.jpg"/> */}
+                <div className='overlay'></div>
                 <div className="container">
                     <h1>Learning Today,</h1>
                     <h1>Leading Tomorrow</h1>
@@ -14,35 +16,29 @@ export default function Landing() {
                         <span className="bg" />
                     </button>
                 </div>
-            </section>
+            </section>  
 
-
-            <section id="about" class="about">
-                <div class="container" data-aos="fade-up">
-
-                    <div class="row">
-                        <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
-                            {/* <!-- <img src="assets/img/about.jpg" class="img-fluid" alt=""/> */}
-                            <img
-                                src="https://img.freepik.com/premium-photo/kids-science-concept-cute-indian-little-kid-student-scientist-studying-science-experimenting-with-microscope-chemicals-with-diagrams-doodles-drawn-green-chalkboard_466689-17297.jpg"
-                                class="img-fluid" alt="" />
-                        </div>
-                        <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+            <section id="about" className="about">
+                <div className="container" data-aos="fade-up">
+                    <div className="row">
+                        <div className="content">
                             <h3>Kranti is an innovative organization that is dedicated to transforming education through:</h3>
-                            <p class="fst-italic">
-
-                            </p>
                             <ul>
-                                <li><i class="bi bi-check-circle"></i> <b>Smart classrooms:</b> Kranti introduces cutting-edge
+                                <li><i class="fa-regular fa-circle-check" style={{color: "#9141ac",}}></i> <b>Smart classrooms:</b> Kranti introduces cutting-edge
                                     technological solutions in classrooms, including interactive whiteboards, projectors, and smart devices,
                                     to create engaging and interactive learning environments.</li>
-                                <li><i class="bi bi-check-circle"></i> <b>Inclusive learning:</b> Kranti is committed to providing equal
+                                <li><i class="fa-regular fa-circle-check" style={{color: "#9141ac",}}></i> <b>Inclusive learning:</b> Kranti is committed to providing equal
                                     educational opportunities to all students, including those with special needs, through an inclusive
                                     learning environment.</li>
-                                <li><i class="bi bi-check-circle"></i> <b>Comprehensive training and support:</b> Kranti provides
+                                <li><i class="fa-regular fa-circle-check" style={{color: "#9141ac",}}></i> <b>Comprehensive training and support:</b> Kranti provides
                                     latest technological tools and equipment, and
                                     assists educational institutions in planning and implementing technological infrastructure.</li>
                             </ul>
+                        </div>
+                        <div className="img2_container" data-aos="fade-left" data-aos-delay="100">
+                            <img
+                                src="https://img.freepik.com/premium-photo/kids-science-concept-cute-indian-little-kid-student-scientist-studying-science-experimenting-with-microscope-chemicals-with-diagrams-doodles-drawn-green-chalkboard_466689-17297.jpg"
+                                className="img2" alt="" />
                         </div>
                     </div>
 
@@ -51,57 +47,33 @@ export default function Landing() {
 
             <section id="footer">
 
-                <footer className="padding_4x">
-                    <div className="flex">
-                        <section className="flex-content padding_1x">
-                            <h3>Top Products</h3>
-                            <a href="#">Managed Website</a>
-                            <a href="#">Manage Reputation</a>
-                            <a href="#">Power Tools</a>
-                            <a href="#">Marketing Service</a>
-                        </section>
-                        <section className="flex-content padding_1x">
-                            <h3>Quick Links</h3>
-                            <a href="#">Jobs</a>
-                            <a href="#">Brand Assets</a>
-                            <a href="#">Investor Relations</a>
-                            <a href="#">Terms of Service</a>
-                        </section>
-                        <section className="flex-content padding_1x">
-                            <h3>Features</h3>
-                            <a href="#">Jobs</a>
-                            <a href="#">Brand Assets</a>
-                            <a href="#">Investor Relations</a>
-                            <a href="#">Terms of Service</a>
-                        </section>
-                        <section className="flex-content padding_1x">
-                            <h3>Resources</h3>
-                            <a href="#">Guides</a>
-                            <a href="#">Research</a>
-                            <a href="#">Experts</a>
-                            <a href="#">Agencies</a>
-                        </section>
-                        <section className="flex-content padding_1x">
-                            <h3>Newsletter</h3>
-                            <p>You can trust us. we only send promo offers,</p>
-                            <fieldset className="fixed_flex">
-                                <input type="email" name="newsletter" placeholder="Your Email Address" />
-                                <button className="btn btn_2">Subscribe</button>
-                            </fieldset>
-                        </section>
-                    </div>
-                    <div className="flex">
-                        <section className="flex-content padding_1x">
-                            {/* <p>Copyright ©2023 All rights reserved || website name</p> */}
-                        </section>
-                        <section className="flex-content padding_1x">
-                            <a href="#"><i className="fa fa-facebook"></i></a>
-                            <a href="#"><i className="fa fa-twitter"></i></a>
-                            <a href="#"><i className="fa fa-dribbble"></i></a>
-                            <a href="#"><i className="fa fa-linkedin"></i></a>
-                        </section>
+                <footer className="footer_content">
+                    <div className='container'>
+                        <div className='row_footer'>
+
+                            <div className='footer_contact'>
+                                <h3>Kranti</h3>
+                                <p>83, CRK Nivas<br />
+                                Chittorghar, Rajasthan 312001<br />
+                                India<br /><br />
+                                <strong>Phone:</strong>1234567890<br />
+                                <strong>Email:</strong>krnatieducation@gmail.com<br />
+                                </p>
+                            </div>
+
+                            <div className='footer_links'>
+                                <h4>Useful Links</h4>
+                                <ul>
+                                    <li><i class="fa-solid fa-chevron-right" style={{color: "#813d9c"}}></i><a href="/">Home</a></li>
+                                    <li><i class="fa-solid fa-chevron-right" style={{color: "#813d9c"}}></i><a href="/">About us</a></li>
+                                    <li><i class="fa-solid fa-chevron-right" style={{color: "#813d9c"}}></i>Terms of Servises</li>
+                                    <li><i class="fa-solid fa-chevron-right" style={{color: "#813d9c"}}></i>Privacy policy</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </footer>
+                
             </section>
 
         </div>
